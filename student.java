2f9b520188a3c916dfdaaceffffa8623922911b2
Rajshekhar <rajzzgs@gmail.com>
2024-03-11 15:32:31 +0530

    the main code branch

---
+++ student.java
@@ -0,0 +1,19 @@
+public class student{
+    static string name;
+    static int age;
+
+public static void main(String[] args) {
+    String name;
+    System.out.println("Student name:" +name);
+    System.out.println("Age of student :" +age);
+}
+public void display(){
+    System.out.println(name.display);
+    System.out.println(age);
+
+}
+{
+    student s1 = new student();
+    s1.display();
+}
+}
